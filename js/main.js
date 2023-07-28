3c6720efd3a9f4f6cff8a4263256d22e49b430c1
chouarifatma <chouarifatma03@gmail.com>
2023-07-28 01:55:02 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function abon(){
+    var name=parseFloat(document.getElementById("name").value)
+    var email=parseFloat(document.getElementById("email").value)
+    var abb=name + email
+    document.getElementById("S'inscrire").innerHTML="S'inscrire" +abb;
+}
+
+        
